c9b08492a2eaf1d43146e5b4d93f5f86e1959fad
Adam Frej <awfrej@googlemail.com>
2019-01-28 11:03:54 -0500

    first commit - frontend routing complete + styled components working

---
+++ frontend/pages/_document.js
@@ -0,0 +1,26 @@
+// set initial props with next to prevent styled-components CSS flicker on page load
+import Document, { Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+export default class MyDocument extends Document {
+  static getInitialProps ({ renderPage }) {
+    const sheet = new ServerStyleSheet()
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
+    const styleTags = sheet.getStyleElement()
+    return { ...page, styleTags }
+  }
+
+  render () {
+    return (
+      <html>
+        <Head>
+          {this.props.styleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
---
+++ frontend/pages/sell.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+const Sell = props => (
+
+  <div>
+
+    <h1>Sell your items here!</h1>
+
+  </div>
+
+)
+
+export default Sell
